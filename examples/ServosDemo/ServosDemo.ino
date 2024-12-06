9e646363f68234ba01c20a64f8ea55eff0a97932
dani-carbonell <danicarbonellrubio@gmail.com>
2024-12-07 00:08:40 +0100

    New branch to set Vendor and Product

--- examples/ServosDemo/ServosDemo.ino
+++ examples/ServosDemo/ServosDemo.ino
@@ -13,6 +13,15 @@
 */
 
 #include <Adafruit_TinyUSB.h>
+
+// Configure USB MIDI
+#if defined(ARDUINO_ARCH_ESP32)
+// Must be before other USB declarations
+USBDevice.setID(0x239A, 0x8022);
+USBDevice.setManufacturerDescriptor("RXM");
+USBDevice.setProductDescriptor("Damping Controller");
+#endif
+
 #include <MIDI.h>
 #include <MIDIServoController.h>
 
@@ -26,44 +35,46 @@ MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MIDI);
 MIDIServoController servoController;
 
 void setup() {
-    
-    Serial.begin(115200);
-    //Debug::begin(Serial, Debug::DEBUG_DEBUG);
-    
-    usb_midi.setStringDescriptor("Servo Controller");
-    usb_midi.begin();
-    
-    //delay(1000);
+  Serial.begin(115200);
+  
+  // Configure USB MIDI
+  TinyUSBDevice.setID(0x239A, 0x8022); //Change this to your own VID/PID
+  TinyUSBDevice.setProductDescriptor("Servo Controller"); //Change this to your own product name
 
-    // Allocate PWM timers (for ESP32)
-    ESP32PWM::allocateTimer(0);
-    ESP32PWM::allocateTimer(1);
-    ESP32PWM::allocateTimer(2);
-    ESP32PWM::allocateTimer(3);
+  usb_midi.setStringDescriptor("Servos");
+  usb_midi.begin();
+  
+  //delay(1000);
 
-    // Configure servos
-    servoController.begin(MIDI);
-    
-    // Set pins and limits for each servo
-    servoController.setServoPin(0, 3, 500, 2500, 1500);  // Servo 1
-    servoController.setServoPin(1, 5, 500, 2500, 1500);  // Servo 2
-    servoController.setServoPin(2, 7, 500, 2500, 1500);  // Servo 3
-    servoController.setServoPin(3, 9, 500, 2500, 1500);  // Servo 4
-    servoController.setServoPin(4, 11, 500, 2500, 1500); // Servo 5
-    servoController.setServoPin(5, 2, 500, 2500, 1500); // Servo 6
-    servoController.setServoPin(6, 4, 500, 2500, 1500); // Servo 7
-    servoController.setServoPin(7, 6, 500, 2500, 1500); // Servo 8
+  // Allocate PWM timers (for ESP32)
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
 
-    // Set 14-bit CC numbers for servos
-    // Position (Coarse, Fine), Speed
-    servoController.setServoCCs(0, 20, 52, 30);  // Servo 1
-    servoController.setServoCCs(1, 21, 53, 31);  // Servo 2
-    servoController.setServoCCs(2, 22, 54, 32);  // Servo 3
-    servoController.setServoCCs(3, 23, 55, 33);  // Servo 4
-    servoController.setServoCCs(4, 24, 56, 34);  // Servo 5
-    servoController.setServoCCs(5, 25, 57, 35);  // Servo 6
-    servoController.setServoCCs(6, 26, 58, 36);  // Servo 7
-    servoController.setServoCCs(7, 27, 59, 37);  // Servo 8
+  // Configure servos
+  servoController.begin(MIDI);
+  
+  // Set pins and limits for each servo
+  servoController.setServoPin(0, 3, 500, 2500, 1500);  // Servo 1
+  servoController.setServoPin(1, 5, 500, 2500, 1500);  // Servo 2
+  servoController.setServoPin(2, 7, 500, 2500, 1500);  // Servo 3
+  servoController.setServoPin(3, 9, 500, 2500, 1500);  // Servo 4
+  servoController.setServoPin(4, 11, 500, 2500, 1500); // Servo 5
+  servoController.setServoPin(5, 2, 500, 2500, 1500); // Servo 6
+  servoController.setServoPin(6, 4, 500, 2500, 1500); // Servo 7
+  servoController.setServoPin(7, 6, 500, 2500, 1500); // Servo 8
+
+  // Set 14-bit CC numbers for servos
+  // Position (Coarse, Fine), Speed
+  servoController.setServoCCs(0, 20, 52, 30);  // Servo 1
+  servoController.setServoCCs(1, 21, 53, 31);  // Servo 2
+  servoController.setServoCCs(2, 22, 54, 32);  // Servo 3
+  servoController.setServoCCs(3, 23, 55, 33);  // Servo 4
+  servoController.setServoCCs(4, 24, 56, 34);  // Servo 5
+  servoController.setServoCCs(5, 25, 57, 35);  // Servo 6
+  servoController.setServoCCs(6, 26, 58, 36);  // Servo 7
+  servoController.setServoCCs(7, 27, 59, 37);  // Servo 8
 }
 
 void loop() {
